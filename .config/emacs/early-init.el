2d07a2ff76ada0adfe3fc96289f58c4f2555ac85
Alex Gravem <kartesus@gmail.com>
2024-12-31 16:43:41 +0000

    improve emacs config

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,14 +1,3 @@
-;;;  ________                                                _______                 __                            __
-;;; /        |                                              /       \               /  |                          /  |
-;;; $$$$$$$$/ _____  ____   ______   _______  _______       $$$$$$$  | ______   ____$$ | ______   ______   _______$$ |   __
-;;; $$ |__   /     \/    \ /      \ /       |/       |      $$ |__$$ |/      \ /    $$ |/      \ /      \ /       $$ |  /  |
-;;; $$    |  $$$$$$ $$$$  |$$$$$$  /$$$$$$$//$$$$$$$/       $$    $$</$$$$$$  /$$$$$$$ /$$$$$$  /$$$$$$  /$$$$$$$/$$ |_/$$/
-;;; $$$$$/   $$ | $$ | $$ |/    $$ $$ |     $$      \       $$$$$$$  $$    $$ $$ |  $$ $$ |  $$/$$ |  $$ $$ |     $$   $$<
-;;; $$ |_____$$ | $$ | $$ /$$$$$$$ $$ \_____ $$$$$$  |      $$ |__$$ $$$$$$$$/$$ \__$$ $$ |     $$ \__$$ $$ \_____$$$$$$  \
-;;; $$       $$ | $$ | $$ $$    $$ $$       /     $$/       $$    $$/$$       $$    $$ $$ |     $$    $$/$$       $$ | $$  |
-;;; $$$$$$$$/$$/  $$/  $$/ $$$$$$$/ $$$$$$$/$$$$$$$/        $$$$$$$/  $$$$$$$/ $$$$$$$/$$/       $$$$$$/  $$$$$$$/$$/   $$/
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;   Basic settings for quick startup and convenience
@@ -27,16 +16,12 @@
 
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (setq frame-resize-pixelwise t)
-(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+(tool-bar-mode -1)
 (setq default-frame-alist '((fullscreen . maximized)
 
-                            ;; You can turn off scroll bars by uncommenting these lines:
-                            ;; (vertical-scroll-bars . nil)
-                            ;; (horizontal-scroll-bars . nil)
+;; You can turn off scroll bars by uncommenting these lines:
+(vertical-scroll-bars . nil)
+(horizontal-scroll-bars . nil)
 
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (foreground-color . "#ffffff")
-                            (ns-appearance . dark)
-                            (ns-transparent-titlebar . t)))
+(ns-appearance . dark)
+(ns-transparent-titlebar . t)))
--- .config/emacs/extras/org.el
+++ .config/emacs/extras/org.el
@@ -1,80 +1,22 @@
-;;; Emacs Bedrock
-;;;
-;;; Extra config: Org-mode starter config
-
-;;; Usage: Append or require this file from init.el for some software
-;;; development-focused packages.
-;;;
-;;; Org-mode is a fantastically powerful package. It does a lot of things, which
-;;; makes it a little difficult to understand at first.
-;;;
-;;; We will configure Org-mode in phases. Work with each phase as you are
-;;; comfortable.
-;;;
-;;; YOU NEED TO CONFIGURE SOME VARIABLES! The most important variable is the
-;;; `org-directory', which tells org-mode where to look to find your agenda
-;;; files.
-
-;;; See "org-intro.txt" for a high-level overview.
-
-;;; Contents:
-;;;
-;;;  - Critical variables
-;;;  - Phase 1: editing and exporting files
-;;;  - Phase 2: todos, agenda generation, and task tracking
-;;;  - Phase 3: extensions (org-roam, etc.)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;   Critical variables
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; These variables need to be set for Org-mode's full power to be unlocked!
-;;;
-;;; You can read the documentation for any variable with `C-h v'. If you have
-;;; Consult configured (see the `base.el' file) then it should help you find
-;;; what you're looking for.
-
-;;; Phase 1 variables
-
-;;; Phase 2 variables
+;;; You can read the documentation for any variable with `C-h v'.
 
 ;; Agenda variables
-(setq org-directory "~/Documents/org/") 
-(setq org-agenda-files '("inbox.org" "work.org"))
-
-;; Default tags
-(setq org-tag-alist '(
-                      ;; locale
-                      (:startgroup)
-                      ("home" . ?h)
-                      ("work" . ?w)
-                      (:endgroup)
-                      (:newline)
-                      ;; scale
-                      (:startgroup)
-                      ("one-shot" . ?o)
-                      ("project" . ?j)
-                      ("tiny" . ?t)
-                      (:endgroup)
-                      ;; misc
-                      ("meta")
-                      ("review")
-                      ("reading")))
-
-;; Org-refile: where should org-refile look?
-(setq org-refile-targets 'FIXME)
-
-;;; Phase 3 variables
+(setq org-directory "~/Documents/org/prod") 
+(setq org-agenda-files '("personal.org" "work.org"))
 
 ;; Org-roam variables
-(setq org-roam-directory "~/Documents/org-roam/")
-(setq org-roam-index-file "~/Documents/org-roam/index.org")
+(setq org-roam-directory "~/Documents/org/notes/")
+(setq org-roam-index-file "~/Documents/org/notes/index.org")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;;   Phase 1: editing and exporting files
+;;;   General config
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -84,95 +26,58 @@
 
   :bind (:map global-map
               ("C-c l s" . org-store-link)          ; Mnemonic: link → store
-              ("C-c l i" . org-insert-link-global)) ; Mnemonic: link → insert
+              ("C-c l i" . org-insert-link-global)  ; Mnemonic: link → insert
+              ("C-c c" . org-capture)
+              ("C-c a" . org-agenda)) 
   :config
-  (require 'oc-csl)                     ; citation support
-  (add-to-list 'org-export-backends 'md)
-
   ;; Make org-open-at-point follow file links in the same window
   (setf (cdr (assoc 'file org-link-frame-setup)) 'find-file)
 
   ;; Make exporting quotes better
-  (setq org-export-with-smart-quotes t))
+  (setq org-export-with-smart-quotes t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Phase 2: todos, agenda generation, and task tracking
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Yes, you can have multiple use-package declarations. It's best if their
-;; configs don't overlap. Once you've reached Phase 2, I'd recommend merging the
-;; config from Phase 1. I've broken it up here for the sake of clarity.
-(use-package org
-  :config
-  ;; Instead of just two states (TODO, DONE) we set up a few different states
-  ;; that a task can be in. Run
-  ;;     M-x describe-variable RET org-todo-keywords RET
-  ;; for documentation on how these keywords work.
+  ;; Logging options
+  (setq org-log-into-drawer "LOGBOOK")
+  (setq org-log-reschedule 'note)
+  
+  ;; Tasks
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "WAITING(w@/!)" "STARTED(s!)" "|" "DONE(d!)" "OBSOLETE(o@)")))
+        '((sequence "TODO(t!)" "READ(r!)" "WAIT(w@/!)" "WORK(k!)" "|" "DONE(d!)" "VOID(v@/!)")))
 
   ;; Refile configuration
+  (setq org-refile-targets '((org-agenda-files :level . 1)))
   (setq org-outline-path-complete-in-steps nil)
   (setq org-refile-use-outline-path 'file)
+  (setq org-refile-allow-creating-parent-nodes t)
 
+  ;; Task dependencies
+  (setq org-enforce-todo-dependencies t)
+  (setq org-enforce-todo-checkbox-dependencies t)
+  (setq org-agenda-dim-blocked-tasks t)
+  
   (setq org-capture-templates
         '(("c" "Default Capture" entry (file "inbox.org")
-           "* TODO %?\n%U\n%i")
-          ;; Capture and keep an org-link to the thing we're currently working with
+           "* TODO %^{Title}\n:LOGBOOK:\n- Created: %U\n:END:\n%?")
           ("r" "Capture with Reference" entry (file "inbox.org")
-           "* TODO %?\n%U\n%i\n%a")
-          ;; Define a section
-          ("w" "Work")
-          ("wm" "Work meeting" entry (file+headline "work.org" "Meetings")
-           "** TODO %?\n%U\n%i\n%a")
-          ("wr" "Work report" entry (file+headline "work.org" "Reports")
-           "** TODO %?\n%U\n%i\n%a")))
-
-  ;; An agenda view lets you see your TODO items filtered and
-  ;; formatted in different ways. You can have multiple agenda views;
-  ;; please see the org-mode documentation for more information.
-  (setq org-agenda-custom-commands
-        '(("n" "Agenda and All Todos"
-           ((agenda)
-            (todo)))
-          ("w" "Work" agenda ""
-           ((org-agenda-files '("work.org")))))))
+           "* TODO %^{Title}\n:LOGBOOK:\n- Created: %U\n:REFERENCE: %a\n:END:\n%?"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;;   Phase 3: extensions
+;;;   extensions
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Resize Org headings
-(dolist (face '((org-level-1 . 1.35)
-                (org-level-2 . 1.3)
-                (org-level-3 . 1.2)
-                (org-level-4 . 1.1)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Overpass Nerd Font" :weight 'bold :height (cdr face)))
 
 (use-package org
   :hook ((org-mode-hook . variable-pitch-mode)
          (org-mode-hook . org-indent-mode))
   :config
-  (set-face-attribute 'org-document-title nil   :font "Overpass Nerd Font" :weight 'bold :height 1.8)
-  (set-face-attribute 'org-block nil            :foreground nil :inherit 'fixed-pitch :height 0.85)
-  (set-face-attribute 'org-code nil             :inherit '(shadow fixed-pitch) :height 0.85)
-  (set-face-attribute 'org-verbatim nil         :inherit '(shadow fixed-pitch) :height 0.85)
-  (set-face-attribute 'org-special-keyword nil  :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil        :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil         :inherit 'fixed-pitch)
+  (require 'org-habit)
+  (setq org-habit-show-habits-only-for-today t)
 
   (setq org-adapt-indentation t
         org-hide-leading-stars t
         org-pretty-entities t
-	    org-ellipsis "  ·")
+	    org-ellipsis "·")
 
   (setq org-src-fontify-natively t
 	    org-src-tab-acts-natively t
@@ -184,11 +89,12 @@
   (setq org-superstar-leading-bullet " ")
   (setq org-superstar-headline-bullets-list '("◉" "○" "⚬" "◈" "◇"))
   (setq org-superstar-special-todo-items t) ;; Makes TODO header bullets into boxes
-  (setq org-superstar-todo-bullet-alist '(("TODO"  . 9744)
-                                          ("WAIT"  . 9744)
-                                          ("READ"  . 9744)
-                                          ("PROG"  . 9744)
-										  ("DONE"  . 9745)))
+  (setq org-superstar-todo-bullet-alist '(("TODO"  . ?⛶)
+                                          ("READ"  . ?⛶)
+                                          ("WAIT"  . ?⍰)
+                                          ("WORK"  . ?☐)
+                                          ("VOID"  . ?☒)
+										  ("DONE"  . ?☑)))
   :hook (org-mode . org-superstar-mode))
 
 (use-package org-roam
